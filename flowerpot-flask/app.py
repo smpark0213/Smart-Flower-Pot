38c26cb4c0ec4845d18efe7751baa18d13a94873
Ucan <zyzy1231@naver.com>
2022-06-03 22:10:55 +0900

    feat : multiprocessing code

--- flowerpot-flask/app.py
+++ flowerpot-flask/app.py
@@ -1,89 +1,167 @@
-import json
+from distutils.util import run_2to3
+import multiprocessing
+import os
+import time
 import eventlet
-from flask import Flask, render_template, jsonify
+from multiprocessing import Process
+from multiprocessing.dummy import Process
+from flask import Flask, jsonify
 from flask_mqtt import Mqtt
-from flask_bootstrap import Bootstrap
-# from flask_socketio import SocketIO
+from gpiozero import Motor
+from flask_socketio import SocketIO
 
 broker_ip = '127.0.0.1'
 mqtt_port = 1883
 app = Flask(__name__)
+
 app.config['MQTT_BROKER_URL'] = broker_ip
 app.config['MQTT_BROKER_PORT'] = mqtt_port
 # app.config['MQTT_USERNAME'] = 'user'
 # app.config['MQTT_PASSWORD'] = 'secret'
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 
-mqtt = Mqtt(app)
-# socketio = SocketIO(app)
+def run_snesor(queue, errorDict):
+    motor1 = Motor(forward=17, backward=27)
+    motor2 = Motor(forward=22, backward=23)
+    motor3 = Motor(forward=5, backward=6)
+    motor4 = Motor(forward=13, backward=19)
 
-class Pot:
-    def __init__(self, topic, moisture, light):
-        self.topic = topic
-        self.moisture = moisture
-        self.light = light
+    # 전진
+    @app.route('/forward')
+    def move_forward():
+        try:
+            print('High')
+            motor1.forward()
+            motor2.forward()
+            motor3.forward()
+            motor4.forward()
+            time.sleep(5)
+        except:
+            print("Error")
+            return jsonify(
+                code=500,
+                success=False,
+                msg='Sensor Connection Error'
+            )
+
+        finally:
+            motor1.stop()
+            motor2.stop()
+            motor3.stop()
+            motor4.stop()
+            return jsonify(
+                code=200,
+                success=True,
+                msg='OK'
+            )
+            
+
+    # 후진
+    @app.route('/back')
+    def move_backward():
+        try:
+            print('Low')
+            motor1.backward()
+            motor2.backward()
+            motor3.backward()
+            motor4.backward()
+            time.sleep(5)
+        except:
+            print("Error")
+            return jsonify(
+                code=500,
+                success=False,
+                msg='Sensor Connection Error'
+            )
+
+        finally:
+            motor1.stop()
+            motor2.stop()
+            motor3.stop()
+            motor4.stop()
+            return jsonify(
+                code=200,
+                success=True,
+                msg='OK'
+            )
 
 
-topic = ['flowerpot1', 'flowerpot2', 'flowerpot3']
-global_data = {'flowerpot1': {}, 'flowerpot2': {}, 'flowerpot3': {}}
+def run_flask(queue, errorDict):
+    socketio = SocketIO(app)
+    mqtt = Mqtt(app)
+
+    topic = ['flowerpot1', 'flowerpot2', 'flowerpot3']
+    flowerpot_data = {'flowerpot1': {}, 'flowerpot2': {}, 'flowerpot3': {}}
+
+    @app.route('/')
+    def index():
+        return jsonify({"index" : 'Hello world!'})
+
+    @mqtt.on_connect()
+    def handle_connect(client, userdata, flags, rc):
+        if(rc == 0):
+            for t in topic :
+                mqtt.subscribe(t)
+        else:
+            print("Bad connection Returned code = ", rc)
 
 
-@app.route('/')
-def index():
-    return jsonify({"index" : 'Hello world!'})
-
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    if(rc == 0):
-        for t in topic :
-            mqtt.subscribe(t)
-    else:
-        print("Bad connection Returned code = ", rc)
+    @mqtt.on_message()
+    def handle_mqtt_message(client, userdata, message):
+        list=message.payload.decode().split()
+        flowerpot_data
+        # Moisture <int> / Light <float>
+        # 0         1   2   3       4
+        moisture = float(list[1])
+        light = float(list[4])
+        global_pot_update(message.topic, moisture, light)
 
 
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    list=message.payload.decode().split()
-    global global_data
-    # Moisture <int> / Light <float>
-    # 0         1   2   3       4
-    moisture = float(list[1])
-    light = float(list[4])
-    global_pot_update(message.topic, moisture, light)
-    # data = dict(
-    #     topic=message.topic,
-    #     moisture= moisture,
-    #     light= light
-    # )
-    # socketio.emit('mqtt_message', data=data)
+    def global_pot_update(inputTopic, moisture, light):
+        flowerpot_data
+        topic
+        print(inputTopic)
+        if(inputTopic == topic[0]):
+            flowerpot_data[topic[0]]['moisture'] = moisture
+            flowerpot_data[topic[0]]['light'] = light
+        elif(inputTopic == topic[1]):
+            flowerpot_data[topic[1]]['moisture'] = moisture
+            flowerpot_data[topic[1]]['light'] = light
+        elif(inputTopic == topic[2]):
+            flowerpot_data[topic[2]]['moisture'] = moisture
+            flowerpot_data[topic[2]]['light'] = light
+        else:
+            return 0
+        print(flowerpot_data)
+
+    @app.route('/flowerpot')
+    def get_flowerpot1():
+        flowerpot_data
+        return jsonify(flowerpot_data)
 
 
-def global_pot_update(inputTopic, moisture, light):
-    global global_data
-    global topic
-    print(inputTopic)
-    if(inputTopic == topic[0]):
-        global_data[topic[0]]['moisture'] = moisture
-        global_data[topic[0]]['light'] = light
-    elif(inputTopic == topic[1]):
-        global_data[topic[1]]['moisture'] = moisture
-        global_data[topic[1]]['light'] = light
-    elif(inputTopic == topic[2]):
-        global_data[topic[2]]['moisture'] = moisture
-        global_data[topic[2]]['light'] = light
-    else:
-        return 0
-    print(global_data)
-    return 1
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
 
-
-@app.route('/flowerpot')
-def get_flowerpot1():
-    global global_data
-    return jsonify(global_data)
 
 if __name__ == '__main__':
     # important: Do not use reloader because this will create two Flask instances.
     # Flask-MQTT only supports running with one instance
-    # socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
-    app.run(host='0.0.0.0', debug=True)
+    print("Flower-Pot-Server Runing...\n Pid of main : ", os.getpid())
+
+    #MultiProcessor간 변수 공유를 위한 Manager
+    manager = multiprocessing.Manager()
+
+    # 명령 Queue
+    queueList = manager.list()
+    # Error Check용 Dict
+    errorDict = manager.dict({'isError': False})
+
+    # Sensor Process
+    flask_process = Process(target=run_flask, args=(queueList, errorDict))
+    sensor_process = Process(target=run_snesor, args=(queueList, errorDict))
+
+    flask_process.start()
+    sensor_process.start()
+
+    flask_process.join()
+    sensor_process.join()
